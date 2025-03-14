48d48540740e663470bc40ca54e4664e1e722219
seltileng <seltilengdabuk@gmail.com>
2025-03-14 15:50:17 +0100

    initial upload on weather suggestion

---
+++ app.py
@@ -0,0 +1,13 @@
+is_hot = True
+if is_hot:
+    print("its a hot day")
+    print("drink plenty of water")
+else:
+    print("its a cold day  ")
+    print("enjoy your day ")
+is_hot = False
+if is_hot:
+    print("its a cold day ")
+    print("stay warm")
+input(is_hot)
+input(is_)
