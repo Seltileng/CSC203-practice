5edea9618bf80cc0dec8c7bbae67bf4a87856efb
Seltileng Dabuk <101059104+Seltileng@users.noreply.github.com>
2025-04-09 10:02:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 is_hot = True
+is_cold = True
+is_raining = True
 if is_hot:
     print("its a hot day")
     print("drink plenty of water")
@@ -9,5 +11,14 @@ is_hot = False
 if is_hot:
     print("its a cold day ")
     print("stay warm")
+if is_cold:
+    print("it's a cold day")
+    print("enjoy")
+else:
+    print("Wear a cold gear")
+    print("stay safe")
 input(is_hot)
-input(is_)
+input(is_cold)
+
+
+
